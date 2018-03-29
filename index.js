9befdc01be9c5c5469037fdbc21fbf8c2b5ff1ba
Anna Gomez <anna.gomez.op@gmail.com>
2018-03-29 16:22:25 +0200

    connection db mongo

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 // Cargar el modulo HTTP
 var http = require('http');
+
+var MongoClient = require('mongodb').MongoClient
+    , format = require('util').format;
+MongoClient.connect('mongodb://localhost:27017/DS', function (err, db) {
+    if (err) {
+        throw err;
+    } else {
+        console.log("successfully connected to the database");
+    }
+    db.close();
+});
  
 // Configurar una respuesta HTTP para todas las peticiones
 function onRequest(request, response) {
+
   console.log("Request received");
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write("Hello world");
