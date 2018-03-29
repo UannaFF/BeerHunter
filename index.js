01dcf8e6c4e7188920aa69b047373fc195a43f59
Anna Gomez <anna.gomez.op@gmail.com>
2018-03-29 15:36:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// Cargar el modulo HTTP
+var http = require('http');
+ 
+// Configurar una respuesta HTTP para todas las peticiones
+function onRequest(request, response) {
+  console.log("Request received");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hello world");
+  response.end();
+}
+ 
+var server = http.createServer(onRequest);
+ 
+// Escuchar al puerto 8080
+server.listen(8080);
+ 
+// Poner un mensaje en la consola
+console.log("Server starsted in http://localhost:8080/");
