cf790cc80f2d3cbf3fe2fcb403d705a1692ed071
UannaFF <anna.gomez.op@gmail.com>
2018-04-12 12:14:23 +0200

    add another node to the replica set

--- replicaset_configuration/erase-replicaset.bash
+++ replicaset_configuration/erase-replicaset.bash
@@ -3,3 +3,4 @@
 rm -rf ./beerhunterreplica/mongodb/beerhunterreplica-0/*
 rm -rf ./beerhunterreplica/mongodb/beerhunterreplica-1/*
 rm -rf ./beerhunterreplica/mongodb/beerhunterreplica-2/*
+rm -rf ./beerhunterreplica/mongodb/beerhunterreplica-3/*
--- replicaset_configuration/linux/confscript.bash
+++ replicaset_configuration/linux/confscript.bash
@@ -8,7 +8,7 @@ if [ ! -d "./mongodb" ]; then
     mkdir mongodb
 fi
 cd mongodb
-for i in 0 1 2
+for i in 0 1 2 3
 do
     if [ ! -d "./beerhunterreplica-$i" ]; then
         echo "Creating directory for replica $i" &
@@ -23,3 +23,5 @@ echo "Setting up second replica set node in port 27018:" &
 xterm -hold -e "mongod --replSet beerhunterreplica --port 27018 --bind_ip localhost --dbpath ./beerhunterreplica/mongodb/beerhunterreplica-1 --smallfiles --oplogSize 128" &
 echo "Setting up third replica set node in port 27019:" &
 xterm -hold -e "mongod --replSet beerhunterreplica --port 27019 --bind_ip localhost --dbpath ./beerhunterreplica/mongodb/beerhunterreplica-2 --smallfiles --oplogSize 128" &
+echo "Setting up fourth replica set node in port 27020:" &
+xterm -hold -e "mongod --replSet beerhunterreplica --port 27020 --bind_ip localhost --dbpath ./beerhunterreplica/mongodb/beerhunterreplica-3 --smallfiles --oplogSize 128" &
